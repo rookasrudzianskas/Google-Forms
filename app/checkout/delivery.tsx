c70672794716c1823bb97e0335bdef3a2217ab17
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 19:27:08 +0300

    🎨 style(delivery.tsx): add Card component to DeliveryDetails screen to improve UI 🚀 perf(delivery.tsx): use useTheme hook to dynamically set background color of Card component

--- app/checkout/delivery.tsx
+++ app/checkout/delivery.tsx
@@ -2,10 +2,11 @@
 import {View, Text, TouchableOpacity} from "react-native";
 import React from "react";
 import {useRouter} from "expo-router";
-import {Button} from "react-native-paper";
+import {Button, Card, TextInput, useTheme} from "react-native-paper";
 
 export default function DeliveryDetails () {
   const router = useRouter();
+  const theme = useTheme();
 
   const nextPage = () => {
     router.push('/checkout/payment');
@@ -13,7 +14,35 @@ export default function DeliveryDetails () {
 
   return (
     <View className="flex-1">
-      <Text className="font-semibold text-2xl">Delivery Details</Text>
+      <Card style={{
+        backgroundColor: theme.colors.background
+      }}>
+        <Card.Title
+          title="Delivery Address"
+          titleVariant={'titleLarge'}
+        />
+        <Card.Content style={{gap: 10}}>
+          <TextInput
+            label="Name"
+            placeholder="Name"
+            // value={text}
+            // onChangeText={text => setText(text)}
+            style={{
+              backgroundColor: theme.colors.background
+            }}
+          />
+
+          <TextInput
+            label="Email"
+            placeholder="hey@byrookas.com"
+            // value={text}
+            // onChangeText={text => setText(text)}
+            style={{
+              backgroundColor: theme.colors.background
+            }}
+          />
+        </Card.Content>
+      </Card>
       <Button mode={'contained'} onPress={nextPage} theme={{roundness: 1}}>
         Next
       </Button>
