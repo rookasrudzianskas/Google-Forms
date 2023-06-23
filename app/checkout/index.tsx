ef0783ff23358bbfafbd12bbabfc4817fc8a8b24
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 19:24:50 +0300

    🎨 style(checkout/index.tsx): improve PersonalDetails component UI by adding a Card component with a title and two TextInput components for name and email 🔥 chore(checkout/index.tsx): remove unused imports and variables from PersonalDetails component

--- app/checkout/index.tsx
+++ app/checkout/index.tsx
@@ -1,19 +1,47 @@
 // @ts-nocheck
-import {View, Text, TouchableOpacity} from "react-native";
+import {View, TouchableOpacity} from "react-native";
 import React from "react";
 import {useRouter} from "expo-router";
-import {Button} from "react-native-paper";
+import {Avatar, Button, Card, Text, TextInput, useTheme, withTheme} from 'react-native-paper';
 
 export default function PersonalDetails () {
   const router = useRouter();
-
+  const theme = useTheme();
   const nextPage = () => {
     router.push('/checkout/delivery');
   }
 
   return (
-    <View className="flex-1">
-      <Text className="font-semibold text-2xl">Personal Details</Text>
+    <View className="flex-1" style={{gap: 15}}>
+      <Card style={{
+        backgroundColor: theme.colors.background
+      }}>
+        <Card.Title
+          title="Personal Information"
+          titleVariant={'titleLarge'}
+        />
+        <Card.Content style={{gap: 10}}>
+          <TextInput
+            label="Name"
+            placeholder="Name"
+            // value={text}
+            // onChangeText={text => setText(text)}
+            style={{
+              backgroundColor: theme.colors.background
+            }}
+          />
+
+          <TextInput
+            label="Email"
+            placeholder="hey@byrookas.com"
+            // value={text}
+            // onChangeText={text => setText(text)}
+            style={{
+              backgroundColor: theme.colors.background
+            }}
+          />
+        </Card.Content>
+      </Card>
 
       <Button mode={'contained'} onPress={nextPage} theme={{roundness: 1}}>
         Next
