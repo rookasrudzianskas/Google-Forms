b126125b124f9536c0d93773712735003754ddca
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 18:59:51 +0300

    🎨 style(_layout.tsx): add StatusBar component to improve user experience and provide visual feedback on checkout process status

--- app/checkout/_layout.tsx
+++ app/checkout/_layout.tsx
@@ -1,23 +1,27 @@
 import {Stack} from "expo-router";
+import {StatusBar} from "expo-status-bar";
 
 export default function CheckoutStack () {
   return (
-    <Stack screenOptions={{
-      contentStyle: {
-        padding: 15,
-        backgroundColor: "#f0ebf8", flex: 1
-      },
-      headerStyle: {
-        backgroundColor: '#673ab8'
-      },
-      headerTintColor: '#f0ebf8',
-      headerTitleStyle: {
-        color: '#f0ebf8'
-      }
-    }}>
-      <Stack.Screen name={"index"} options={{ title: "Personal Information"}} />
-      <Stack.Screen name={"delivery"} options={{ title: "Delivery Information"}} />
-      <Stack.Screen name={"payment"} options={{ title: "Payment Information"}} />
-    </Stack>
+    <>
+      <Stack screenOptions={{
+        contentStyle: {
+          padding: 15,
+          backgroundColor: "#f0ebf8", flex: 1
+        },
+        headerStyle: {
+          backgroundColor: '#673ab8'
+        },
+        headerTintColor: '#f0ebf8',
+        headerTitleStyle: {
+          color: '#f0ebf8'
+        }
+      }}>
+        <Stack.Screen name={"index"} options={{ title: "Personal Information"}} />
+        <Stack.Screen name={"delivery"} options={{ title: "Delivery Information"}} />
+        <Stack.Screen name={"payment"} options={{ title: "Payment Information"}} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   )
 }
