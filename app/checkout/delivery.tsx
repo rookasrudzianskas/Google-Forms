a5965b2c055403a673fbab52d9730f50aed9074b
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 18:31:18 +0300

    ✨ feat(checkout): add font styling to checkout pages' headings to improve readability

--- app/checkout/delivery.tsx
+++ app/checkout/delivery.tsx
@@ -2,10 +2,10 @@
 import {View, Text} from "react-native";
 import React from "react";
 
-export default function PersonalDetails () {
+export default function DeliveryDetails () {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-semibold text-2xl">Personal Details</Text>
+      <Text className="font-semibold text-2xl">Delivery Details</Text>
     </View>
   )
 }
--- app/checkout/payment.tsx
+++ app/checkout/payment.tsx
@@ -2,10 +2,10 @@
 import {View, Text} from "react-native";
 import React from "react";
 
-export default function PersonalDetails () {
+export default function PaymentDetails () {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-semibold text-2xl">Personal Details</Text>
+      <Text className="font-semibold text-2xl">Payment Details</Text>
     </View>
   )
 }
