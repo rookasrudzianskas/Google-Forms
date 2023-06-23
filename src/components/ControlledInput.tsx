cb6ce56e437efa1cf2790ef3ff4cc8c6d22a09ab
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 21:23:02 +0300

    ✨ feat(ControlledInput.tsx): add ControlledInput component to handle controlled inputs with react-hook-form and react-native-paper 🔧 chore(ControlledInput.tsx): add ts-nocheck to ignore typescript errors in this file

---
+++ src/components/ControlledInput.tsx
@@ -0,0 +1,42 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {HelperText, TextInput, useTheme} from "react-native-paper";
+import {Control, Controller} from "react-hook-form";
+import {useRouter} from "expo-router";
+
+type ControlledInputProps = {
+  control: Control;
+  name: string;
+} & React.ComponentProps<typeof TextInput>;
+
+const ControlledInput = ({control, name, ...textInputProps}: ControlledInputProps) => {
+  const theme = useTheme();
+  const router = useRouter();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({field: {value, onChange, onBlur}, fieldState: {error, invalid}}) => (
+        <View>
+          <TextInput
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            error={invalid}
+            style={{
+              backgroundColor: theme.colors.background
+            }}
+            {...textInputProps}
+          />
+          <HelperText type="error" visible={invalid}>
+            {error?.message}
+          </HelperText>
+        </View>
+      )}
+    />
+  );
+};
+
+export default ControlledInput;
