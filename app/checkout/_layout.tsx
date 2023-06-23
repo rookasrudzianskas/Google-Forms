764ce8b561d15fc8e278eed58103cd294a80cc1c
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 18:46:01 +0300

    🎨 style(_layout.tsx): add styles to CheckoutStack screenOptions to improve UI and UX

--- app/checkout/_layout.tsx
+++ app/checkout/_layout.tsx
@@ -2,7 +2,19 @@ import {Stack} from "expo-router";
 
 export default function CheckoutStack () {
   return (
-    <Stack>
+    <Stack screenOptions={{
+      contentStyle: {
+        padding: 15,
+        backgroundColor: "#f0ebf8", flex: 1
+      },
+      headerStyle: {
+        backgroundColor: '#673ab8'
+      },
+      headerTintColor: '#f0ebf8',
+      headerTitleStyle: {
+        color: '#f0ebf8'
+      }
+    }}>
       <Stack.Screen name={"index"} options={{ title: "Personal Information"}} />
       <Stack.Screen name={"delivery"} options={{ title: "Delivery Information"}} />
       <Stack.Screen name={"payment"} options={{ title: "Payment Information"}} />
