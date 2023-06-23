19ed7944c95725ea2a1aa64f5b630d60b9a2e01e
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 18:29:00 +0300

    🎨 style(checkout): add missing semicolon to improve code style 🔥 chore(index.js): remove unused code file ✨ feat(index.tsx): add StatusBar component to improve user experience 🔧 chore(tsconfig.json): add expo/tsconfig.base as extends to improve typescript configuration

---
+++ app/checkout/index.tsx
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import {View, Text} from "react-native";
+import React from "react";
+
+export default function PersonalDetails () {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Personal Details</Text>
+    </View>
+  )
+}
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import { StyleSheet, Text, View } from "react-native";
+import {StatusBar} from "expo-status-bar";
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
