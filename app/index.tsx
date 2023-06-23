3c559f7d903bc221dec59732bf33eb8b4db9fad6
Rokas <rookas.rudzenskas@gmail.com>
2023-06-23 18:34:50 +0300

    🎨 style(index.tsx): add padding top and horizontal padding to improve layout ✨ feat(index.tsx): add button to navigate to checkout page using Expo Router 🐛 fix(index.tsx): import useRouter from Expo Router to fix undefined error

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
 // @ts-nocheck
-import { StyleSheet, Text, View } from "react-native";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {StatusBar} from "expo-status-bar";
+import {useRouter} from "expo-router";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 pt-16 px-5 bg-white">
+      <TouchableOpacity activeOpacity={0.7} onPress={() => router.push('/checkout')}>
+        <Text className="text-xl text-gray-500 font-[500]">Go To Checkout</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
