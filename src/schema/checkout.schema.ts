2eec5e7ab9346857b1a5e5450f215d1bbc4d3532
Rokas <rookas.rudzenskas@gmail.com>
2023-06-24 10:35:38 +0300

    🐛 fix(checkout.schema.ts): add password confirmation validation to PersonalInfoSchema to ensure passwords match

--- src/schema/checkout.schema.ts
+++ src/schema/checkout.schema.ts
@@ -7,6 +7,8 @@ export const PersonalInfoSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
   confirmPassword: z.string().min(8)
+}).refine(data => data.password === data.confirmPassword, {
+  message: 'Passwords do not match',
 });
 
 export type PersonalInfo = z.infer<typeof PersonalInfoSchema>;
